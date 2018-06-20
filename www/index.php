c16c82f4002073d06d6b8158bfa40a8682c2693f
ritu2593 <34676146+ritu2593@users.noreply.github.com>
2018-06-20 18:38:51 +0530

    Update index.php

--- www/index.php
+++ www/index.php
@@ -1,6 +1,6 @@
 <html>
 <head>
-	<title>Hello world!</title>
+	<title>Hello everyone..Good morning!</title>
 	<link href='//fonts.googleapis.com/css?family=Open+Sans:400,700' rel='stylesheet' type='text/css'>
 	<style>
 	body {
